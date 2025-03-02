77ff66ec1e1a056230afa01d97585cf36eaadbb6
rbenites <alrrebe@gmail.com>
2025-03-01 21:48:30 -0500

    query provider implemented

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalStateProvider } from "@/context/GlobalStateProvider";
+import { QueryProvider } from "../components/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,21 +19,15 @@ export const metadata: Metadata = {
   description: "Virtual product store",
 };
 
-// const queryClient = new QueryClient();
-
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {/* <QueryClientProvider client={queryClient}> */}
-        <GlobalStateProvider>{children}</GlobalStateProvider>
-        {/* </QueryClientProvider> */}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <QueryProvider>
+          <GlobalStateProvider>{children}</GlobalStateProvider>
+        </QueryProvider>
       </body>
     </html>
   );
--- src/context/GlobalStateContext.tsx
+++ src/context/GlobalStateContext.tsx
@@ -1,10 +1,10 @@
-"use client";
+  "use client";
 
-import { createContext, useState } from 'react';
+  import { createContext, useState } from 'react';
 
-export interface GlobalState {
-  cart: any[];
-  addToCart: (item: any) => void;
-}
+  export interface GlobalState {
+    cart: any[];
+    addToCart: (item: any) => void;
+  }
 
-export const GlobalStateContext = createContext<GlobalState | undefined>(undefined);
+  export const GlobalStateContext = createContext<GlobalState | undefined>(undefined);
